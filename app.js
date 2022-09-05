6f1e6e6e67b02e950119e3623d2203b89d8c7dce
WalkingZ3d <danielrwillis@hotmail.co.uk>
2022-09-05 18:23:18 +0100

    folders added

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const favicon = require('express-favicon');
+
+const app = express();
+
+app.use(cors("*"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static("public"));
+
+const indexRoutes = require('./routes/indexRoutes');
+const calorieRoutes = require('./routes/calorieRoutes');
+
+app.set('view engine', 'ejs');
+
+app.use(favicon(__dirname + '/public/images/favicon.png'));
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Server Instance
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`http://localhost:${port}`))
+
+
+//app.get('*', checkUser); // Middleware that applies to every endpoint
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Routes
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+app.use('/', indexRoutes); // Routes 
+app.use('/', calorieRoutes); // Routes 
+
+
+// 404 page if user naviagtes to endpoint that does not exist
+app.use((req, res) => {
+    res.status(404).render('404')
+})
+
+//exporting
+module.exports = app
